a4a44fd16103f26848398c6c4d521322e291fe99
Oliver Peate <oli@shippedcode.com>
2021-02-21 15:33:36 +0000

    Delete old implementation 🎉

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -2,57 +2,12 @@ def update_quality(items)
   items.each do |item|
     case(item.name)
     when /Aged Brie/i
-      next update_aged_brie(item)
+      update_aged_brie(item)
     when /Sulfuras/i
-      next
     when /Backstage passes/i
-      next update_backstage_passes(item)
+      update_backstage_passes(item)
     else
-      next update_normal_item(item)
-    end
-
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
+      update_normal_item(item)
     end
   end
 end
